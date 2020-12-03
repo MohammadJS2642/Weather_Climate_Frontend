a397babd37033499a6ea55225cb52f4d843dd8e3
MohammadJS2642 <mohammadjs2642@gmail.com>
2020-12-03 13:37:46 +0330

    for showing weather created body directory and components

---
+++ frontend/src/Body/Climate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// todo: add api and another things for showing weather
+function Climate() {
+
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+
+export default Climate;
